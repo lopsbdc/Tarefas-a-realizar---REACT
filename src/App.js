ec7a600862a8974f49fc6ee9fc218c7bdf13dd9c
Matheus Henrique <lopsbdc@gmail.com>
2023-02-02 12:19:09 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,149 @@
+import './App.css';
+import {useState, useEffect} from 'react'
+import {BsTrash, BsBookmarkCheck, BsBookmarkCheckFill} from "react-icons/bs"
+
+const API = "http://localhost:5000";
+
+function App() {
+
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  // Carregar os 'To dos" ao carregar a pagina
+
+  useEffect(() => {
+
+    const loadData = async() => {
+
+      // segura e a pagina esperar carregar os dados
+      setLoading(true)
+
+      const res = await fetch(API + "/todos").then((res) => res.json()).then((data) => data).catch((err) => console.log(err));
+
+      // "solta" a pagina, encerra o carregamento
+      setLoading(false)
+
+      setTodos(res);
+    };
+
+    loadData();
+
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    
+    const todo ={
+      id: Math.random(),
+      title,
+      time,
+      done: false,
+    };
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    //atualiznaod dinamicamente a pagina ao inserir uma nova tarefa
+    setTodos((prevState) => [...prevState, todo]);
+
+    setTitle("");
+    setTime("");
+  };
+
+
+
+  const handleDelete = async (id) => {
+
+    await fetch(API + "/todos/" + id, {
+      method: "DELETE",
+    });
+
+    setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
+  }
+
+
+  const handleEdit = async (todo) => {
+
+    //inverso do status atual da tarefa
+    todo.done = !todo.done
+
+    const data = await fetch(API + "/todos/" + todo.id, {
+      method: "PUT",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    setTodos((prevState) => prevState.map((t) => (t.id === data.id ? (t = data) : t))
+    );
+
+  }
+
+
+  //Evitar mostrar que há 0 tarefas, porque a pagina ainda não terminou de carregar.
+  if(loading) {
+    return <p>Carregando...</p>
+  }
+
+  return (
+    <div className="App">
+
+      <div className="todo-header">
+          <h1> React Todo </h1>
+        </div>
+
+      <div className="form-todo">
+        <h2>Insira a sua próxima tarefa:</h2>
+
+        <form onSubmit={handleSubmit}>
+
+          <div className='form-control'>
+            <label htmlFor='title'>Qual é a tarefa?</label>
+            <input type="text" name="title" placeholder='Titulo da tarefa' 
+            onChange={(e) => setTitle(e.target.value)} value = {title || ""} required
+            />
+          </div>
+
+          <div className='form-control'>
+            <label htmlFor='time'>Duração:</label>
+            <input type="text" name="time" placeholder='Tempo estimado (em horas)' 
+            onChange={(e) => setTime(e.target.value)} value = {time || ""} required
+            />
+          </div>
+
+
+          <input type="submit" value="Criar tarefa" />
+
+        </form>
+      </div>
+
+      <div className="list-todo">
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas pendentes!</p>}
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id}>
+            <h3 className={todo.done ? "todo-done" : ""}>{todo.title}</h3>
+            <p>Duração: {todo.time}</p>
+            <div className="actions">
+              <span onClick={() => handleEdit(todo)}>
+                {todo.done ? <BsBookmarkCheck /> : <BsBookmarkCheckFill />}
+              </span>
+              <BsTrash onClick={() => handleDelete(todo.id)}/>
+            </div>
+          </div>
+        ))}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
